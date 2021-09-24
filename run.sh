a1152d16f4d2ad70bac37fc3a53adb9b1a7a22a5
Stephen Dunn <stdunn@adobe.com>
2021-09-24 07:24:55 -0700

    no idea

---
+++ run.sh
@@ -0,0 +1,10 @@
+
+hugo -D --themesDir ../../ -d ~/projects/hugo-build/ --minify
+# hugo server --themesDir ../..
+
+
+######
+#
+#
+# hugo server --themesDir ../../ --minify -gc -d ~/projects/hugo-build/
+
